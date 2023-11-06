c2b28055d8bc9940ff255708fb4fb65f01db796e
admin@lenocin.co.za <admin@lenocin.co.za>
2023-11-06 20:50:11 +0200

    Navbar and style active link

---
+++ app/NavBar.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    {
+      href: '/',
+      label: 'Dashboard',
+    },
+    {
+      href: '/issues',
+      label: 'Issues',
+    },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              'text-zinc-900': currentPath === link.href,
+              'text-zinc-500': currentPath !== link.href,
+              'hover: text-zinc-800 transition-colors': true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
